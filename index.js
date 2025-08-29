8d7854898a6d9b831bd977dc935b99b9398fde66
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-29 06:53:28 -0300

    Criando a estrutura do express e do handlebars

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+
+const app = express();
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home', { layout: false });
+});
+
+app.listen(3000, () => {
+    console.log("App funcionando!");
+});
